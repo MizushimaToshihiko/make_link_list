8743ab89bf53aae910485473208d4cc34251b7a5
MizushimaToshihiko <pandachan5228@gmail.com>
2024-08-07 14:09:29 +0900

    Add keyword_extractor.py and more

--- keyword_extractor.py
+++ keyword_extractor.py
@@ -6,28 +6,30 @@ import sys
 from selenium import webdriver
 from urllib.parse import urlparse
 
-chromedriver_path = os.environ["CONDA_PREFIX"]+r"\chromedriver.exe"
+chromedriver_path = os.environ["CONDA_PREFIX"] + r"\chromedriver.exe"
 
 cur_dir = os.getcwd()
-download_dir = f'{cur_dir}' + r"\link_list"
+download_dir = f"{cur_dir}" + r"\link_list"
 # URLリストの指定
 url_list = download_dir + "\\_url_list.csv"
+matched_urls_file = download_dir + "\\matched_urls.csv"
 
 chrome_Options = webdriver.ChromeOptions()
-prefs = {'download.default_directory' : download_dir}
-chrome_Options.add_argument('--headless')
-chrome_Options.add_experimental_option('prefs', prefs)
-chrome_Options.add_experimental_option('excludeSwitches', ['enable-logging'])
+prefs = {"download.default_directory": download_dir}
+chrome_Options.add_argument("--headless")
+chrome_Options.add_experimental_option("prefs", prefs)
+chrome_Options.add_experimental_option("excludeSwitches", ["enable-logging"])
 chrome_Options.use_chromium = True
 
 driver = webdriver.Chrome(
     executable_path=chromedriver_path,
     options=chrome_Options,
 )
-driver.set_page_load_timeout(600) # ページロード最大600秒
+driver.set_page_load_timeout(600)  # ページロード最大600秒
 
 CRAWL_RANGE = ""
 NG_CHARS = "#?"
+KEYWORDS = []  # 検索語を格納するリスト
 
 
 def my_index(s, x, default=False):
@@ -76,12 +78,25 @@ def save_links_to_csv(url, links):
     for forb in forbidden:
         url_f = url_f.replace(forb, "_")
 
-    with open(download_dir + "\\" + url_f + ".csv",
-              mode="w", newline="", encoding="cp932", errors="ignore") as c:
+    with open(
+        download_dir + "\\" + url_f + ".csv",
+        mode="w",
+        newline="",
+        encoding="cp932",
+        errors="ignore",
+    ) as c:
         f = csv.writer(c)
         f.writerow([url])
         f.writerow(["リンクテキスト", "URL"])
         f.writerows(links)
+
+
+def save_matched_url(url):
+    """
+    Saves the URL to the matched_urls file.
+    """
+    with open(matched_urls_file, mode="a", newline="") as f:
+        f.write(url + "\n")
 
 
 def extract_links():
@@ -99,10 +114,22 @@ def extract_links():
             img_elements = element.find_elements("xpath", ".//img")
             if img_elements:
                 links.append(
-                    ["[画像]" + img_elements[0].get_attribute("alt"), element.get_attribute("href")])
+                    [
+                        "[画像]" + img_elements[0].get_attribute("alt"),
+                        element.get_attribute("href"),
+                    ]
+                )
             else:
                 links.append([link_text, element.get_attribute("href")])
     return links
+
+
+def contains_keywords():
+    """
+    Check if the page contains specific keywords.
+    """
+    page_source = driver.page_source
+    return any(keyword in page_source for keyword in KEYWORDS)
 
 
 def crawl(url):
@@ -126,6 +153,9 @@ def crawl(url):
         return
     time.sleep(1)
 
+    if contains_keywords():
+        save_matched_url(url)
+
     links = extract_links()
     save_links_to_csv(url, links)
 
@@ -146,7 +176,15 @@ if __name__ == "__main__":
     with open(url_list, mode="w") as f:
         f.truncate(0)
 
+    with open(matched_urls_file, mode="w") as f:
+        f.truncate(0)
+
+    if len(sys.argv) < 3:
+        print("Usage: python script_name.py <URL> <keyword1> [<keyword2> ...]")
+        sys.exit(1)
+
     CRAWL_RANGE = urlparse(sys.argv[1]).netloc
+    KEYWORDS = sys.argv[2:]
     crawl(sys.argv[1])
 
     driver.quit()
